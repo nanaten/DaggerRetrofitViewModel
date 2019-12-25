8aebde7da2336af8da1f38da0d030b1f31eab471
m.coder <kurokoshow00@gmail.com>
2019-12-25 11:49:49 +0900

    fix App (use DaggerApplication)

--- app/src/main/java/com/nanaten/daggerretrofitviewmodel/App.kt
+++ app/src/main/java/com/nanaten/daggerretrofitviewmodel/App.kt
@@ -3,19 +3,16 @@ package com.nanaten.daggerretrofitviewmodel
 import android.app.Application
 import com.nanaten.daggerretrofitviewmodel.di.app.DaggerAppComponent
 import dagger.android.AndroidInjector
+import dagger.android.DaggerApplication
 import dagger.android.DispatchingAndroidInjector
 import dagger.android.HasAndroidInjector
 import javax.inject.Inject
 
-class App : Application(), HasAndroidInjector {
-    @Inject
-    lateinit var dispatchActivityInjector: DispatchingAndroidInjector<Any>
-
-    override fun androidInjector(): AndroidInjector<Any>{
-        DaggerAppComponent.builder()
+class App : DaggerApplication(), HasAndroidInjector {
+    override fun applicationInjector(): AndroidInjector<out DaggerApplication> {
+        return DaggerAppComponent.builder()
             .application(this)
             .build()
-            .inject(this)
-        return dispatchActivityInjector
     }
+
 }
